8837e1f0398f1051421cbb4017cf8b049a67296f
Zynee19 <166179801+Zynee19@users.noreply.github.com>
2026-03-13 18:31:51 +0800

    Create environment_config.php for DB connection
    Add database connection configuration for MySQL.

---
+++ environment_config.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "users";
+
+$conn = mysqli_connect($host,$user,$password
+
+$conn = mysqli_connect($host,$user,$password
+
+if(!$conn){
+die("Connection failed");
+}
+?>
